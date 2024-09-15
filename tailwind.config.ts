d218623037f1485fd8c27adc6816c3d7f921cd8a
Mangesh636 <mangeshbhardwaj007@gmail.com>
2024-09-16 02:00:13 +0530

    Refactor tailwind.config.ts to update plugins and add new functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,11 @@ import type { Config } from "tailwindcss";
 
 import { nextui } from "@nextui-org/react";
 
+import svgToDataUri from "mini-svg-data-uri";
+
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -30,6 +35,41 @@ const config: Config = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            success: {
+              "50": "#f1fdf0",
+              "100": "#defcdc",
+              "200": "#bff7bb",
+              "300": "#8bef86",
+              "400": "#4ddd46",
+              "500": "#29c522",
+              "600": "#1ca316",
+              "700": "#198015",
+              "800": "#186516",
+              "900": "#155314",
+              foreground: "#ffffff",
+            },
+          },
+        },
+      },
+    }),
+    // Plugin for Grid background
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="28" height="28" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
 export default config;
